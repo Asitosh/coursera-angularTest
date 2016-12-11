a555e87f28643f0ce334ac8dee67375f1ad0a0a2
Asitosh <asitoshpathak@gmail.com>
2016-12-12 00:27:35 +0530

    module1_solution

---
+++ module1_solution/app.js
@@ -0,0 +1,24 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.message = "";
+  $scope.lunchMenu = "";
+
+  $scope.checkLunch = function () {
+    if($scope.lunchMenu.split(",").length<=3){
+      $scope.message ="Enjoy!";
+    }else {
+      $scope.message ="Too much!";
+    }
+    //console.console.log("fucYOU");
+  };
+
+
+}
+
+})();
