01dcdbc9035cf5d4391fb4b42b53aa0bd524d966
Asitosh <asitoshpathak@gmail.com>
2016-12-12 00:40:22 +0530

    module1_solution

--- module1_solution/app.js
+++ module1_solution/app.js
@@ -10,7 +10,10 @@ function LunchCheckController($scope) {
   $scope.lunchMenu = "";
 
   $scope.checkLunch = function () {
-    if($scope.lunchMenu.split(",").length<=3){
+    if($scope.lunchMenu.split(",").length==0){
+      $scope.message ="Please enter data first";
+    }
+    if($scope.lunchMenu.split(",").length>0&&$scope.lunchMenu.split(",").length<=3){
       $scope.message ="Enjoy!";
     }else {
       $scope.message ="Too much!";
